01f3fb0446661611f21b197bc300b08020c246ff
epicsledge0510 <38469540+epicsledge0510@users.noreply.github.com>
2023-04-17 08:50:49 -0400

    domomaker-c

--- server/router.js
+++ server/router.js
@@ -1,15 +1,19 @@
 const controllers = require('./controllers');
+const mid = require('./middleware');
 
 const router = (app) => {
-    app.get('/login', controllers.Account.loginPage);
-    app.post('/login', controllers.Account.login);
+  app.get('/login', mid.reqiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', mid.reqiresSecure, mid.requiresLogout, controllers.Account.login);
 
-    app.get('/signup', controllers.Account.signupPage);
-    app.post('/signup', controllers.Account.signup);
+  app.get('/signup', mid.reqiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+  app.post('/signup', mid.reqiresSecure, mid.requiresLogout, controllers.Account.signup);
 
-    app.get('/logout', controllers.Account.logout);
-    app.get('/maker', controllers.Domo.makerPage);
-    app.get('/', controllers.Account.loginPage);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
+
+  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
+
+  app.get('/', mid.reqiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
 module.exports = router;
