37ad67813f1113ad69d53da52193b49b7f586670
Andrew Beach <Epicsledge0510@gmail.com>
2023-04-20 13:51:36 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 
 const router = require('./router.js');
 
-const port = process.env.port || process.env.NODE_PORT || 3001;
+const port = process.env.PORT || process.env.NODE_PORT || 3001;
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
